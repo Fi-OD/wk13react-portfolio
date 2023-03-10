c0529182ad0818b8db86d8d74a8a2edc2003678f
FOD <fionnuala.k.odonnell@gmail.com>
2023-03-10 16:39:40 -0500

    removed button component and added images

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -2,28 +2,37 @@ import React from "react";
 import "./style.css";
 
 
+
+
 function Card(props) {
   //const { title, description, thumbnailUrl, viewUrl, gitHubURL } = props;
 
   return (
- // <div className="col">
-  <div className="card">
-    <img className= "card-img-top image" src={props.thumbnailUrl} alt={props.title} ></img>
-    <div className="cardBody">
-    <h3>{props.title}</h3>
-      <p className="card-text">{props.description}.</p>
-      <div className="d-flex justify-content-between align-items-center">
-        <div className="btn-group">
-          <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-          <button type="button" className="btn btn-sm btn-outline-secondary">Repo</button>
+    // <div className="col">
+    <div className="card">
+      <img className="card-img-top" src={props.thumbnailUrl} alt={props.title} ></img>
+      <div className="cardBody">
+        <h3>{props.title}</h3>
+        <p className="card-text">{props.description}.</p>
+        <div className="d-flex justify-content-between align-items-center">
+          <div className="btn-group">
+            <button className="btn" >
+            <a href={props.viewUrl} target="_blank">Page</a></button>
+          </div>
+          <div>
+          <img src="/images/GitHub_Logo.png"></img>
+          </div>
         </div>
       </div>
+      <div>
+      </div>
     </div>
-  </div>
-//</div>
-);
+    //</div>
+
+
+  );
 }
-    
+
 
 export default Card;
 
